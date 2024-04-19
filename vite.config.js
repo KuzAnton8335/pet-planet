3dddc7f87fc2d962dae54280470a6e9b20b447c6
KuzAnton8335 <ant2783@gmail.com>
2024-04-19 13:13:00 +0300

    big screen

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,31 @@
 import { defineConfig } from 'vite';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
-  root: './src',
-  publicDir: "../public",
+  root: "./src",
+  publicDir: "./public",
   build: {
-    outDir: '../dist',
+    outDir: "../dist",
   },
+  plugins: [
+    ViteImageOptimizer({
+      png: {
+        quality: 80,
+      },
+      jpeg: {
+        quality: 80,
+      },
+      jpg: {
+        quality: 80,
+      },
+      webp: {
+        quality: 80,
+      },
+      avif: {
+        quality: 70,
+      },
+      cache: false,
+      cacheLocation: undefined,
+    }),
+  ],
 })
